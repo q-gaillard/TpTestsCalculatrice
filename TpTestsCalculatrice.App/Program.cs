64d76e425b2fc8f53c51f811247a74b264cd19c5
q-gaillard <quentingaillard@edouardgand.fr>
2026-04-02 15:17:47 +0200

    test calculatriceTest.cs

--- TpTestsCalculatrice.App/Program.cs
+++ TpTestsCalculatrice.App/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Calculatrice calc = new Calculatrice();
+
+        Console.WriteLine(calc.Addition(2, 3));
+        Console.WriteLine(calc.Division(10, 2));
+
+        // testez directement dans le main n'est pas 100% éfficace car il n'est pas facile de vérifier les résultats et de gérer les erreurs
     }
 }
