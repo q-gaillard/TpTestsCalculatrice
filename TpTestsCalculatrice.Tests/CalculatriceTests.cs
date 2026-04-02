3acbdab639380fa30e3327be18129c74e5e2e1db
q-gaillard <quentingaillard@edouardgand.fr>
2026-04-02 15:25:53 +0200

    test soutraction multiplication division

--- TpTestsCalculatrice.Tests/CalculatriceTests.cs
+++ TpTestsCalculatrice.Tests/CalculatriceTests.cs
@@ -10,5 +10,11 @@ public class CalculatriceTests
         Calculatrice calc = new Calculatrice();
         int resultat = calc.Addition(2, 3);
         Assert.AreEqual(5, resultat);
+        resultat = calc.Soustraction(5, 3);
+        Assert.AreEqual(2, resultat);
+        resultat = calc.Multiplication(4, 3);
+        Assert.AreEqual(12, resultat);
+        double resultatDivision = calc.Division(10, 2);
+        Assert.AreEqual(5, resultatDivision);
     }
 }
